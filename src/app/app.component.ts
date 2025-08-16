98686389bae4fb5de1074108572bd4f39c1657cc
Vishal Rathore <vishalrathoremail@gmail.com>
2025-08-17 03:08:49 +0530

    Initial commit - frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'cric-analyst-frontend';
+export class AppComponent implements OnInit {
+  message = 'Loading...';
+
+  // Replace with your backend URL
+  private _apiUrl = 'http://localhost:3000/api/hello';
+  public get apiUrl() {
+    return this._apiUrl;
+  }
+  public set apiUrl(value) {
+    this._apiUrl = value;
+  }
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any>(this.apiUrl).subscribe({
+      next: (res) => this.message = res.message,
+      error: (err) => this.message = 'Error fetching message'
+    });
+  }
 }
